12f8a6ede25df7b26ed8ac0c8c4760cd387e5c1a
typshi-aswin <aswinvsivan@gmail.com>
2025-08-17 17:29:27 +0530

    feat: moved footer image to public

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return (
         <div className={styles.container}>
-            <img src ='src\assets\mascot.png' className={styles.img}/>
+            <img src ='/mascot.png' className={styles.img} alt='footer'/>
             <p className={styles.footerText}> Gamified Icebreaker </p>
         </div>
     )
